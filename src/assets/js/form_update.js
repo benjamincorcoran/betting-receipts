f4980d4906e8125e6e5aed94116a119661a81cc9
Ben Corcoran <benjamin.corcoran@gmail.com>
2023-03-18 15:03:19 +0000

    Add placing

--- src/assets/js/form_update.js
+++ src/assets/js/form_update.js
@@ -1,5 +1,32 @@
 const gbpFormat = new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' })
 
+const randomTransaction = function () {
+    $("#transactionNumber").text(parseInt(Math.random().toString(16).slice(2), 16))
+}
+
+const getRadio = function () {
+    if ($("#winRadio").is(":checked")) {
+        return 'win';
+    }
+
+    else if ($("#eachWayRadio").is(":checked")) {
+        return 'each';
+    }
+}
+
+const updateBetType = function () {
+    $("#winRadio").click(function () {
+        $('.receiptBetType').text("To Win")
+        $(".placeReturnSection").css("display", "none")
+        updateReturn()
+    })
+    $("#eachWayRadio").click(function () {
+        $('.receiptBetType').text("Each Way")
+        $(".placeReturnSection").css("display", "flex")
+        updateReturn()
+    })
+}
+
 const updateReceiptData = function (dataItemName, format = "") {
     $('#' + dataItemName).keyup(function () {
         var enteredValue = $('#' + dataItemName).val();
@@ -17,19 +44,29 @@ const updateReturn = function () {
     var stake = $('.receiptStake').first().text();
     var floatStake = Number(stake.replace(/[^0-9.-]+/g, ""))
     var floatOdds = eval(receiptOdds.replace(":", "/"));
-    console.log([receiptOdds, stake, floatStake, floatOdds])
-    var expectedReturn = floatStake * floatOdds
+    console.log([getRadio(), receiptOdds, stake, floatStake, floatOdds, expectedReturn])
+    if (getRadio() == "each") {
+        var winReturn = floatStake / 2 * floatOdds
+        var placeReturn = floatStake / 2 * floatOdds / 3
+        var expectedReturn = winReturn + placeReturn
+        $(".placeReturn").text(gbpFormat.format(placeReturn))
+    } else {
+        var expectedReturn = floatStake * floatOdds
+    }
+    console.log([getRadio(), receiptOdds, stake, floatStake, floatOdds, expectedReturn])
     $(".betReturnValue").text(gbpFormat.format(expectedReturn))
 
 }
 
 $(document).ready(function () {
     $('.date').text(moment().format('DD/MM/YYYY'))
+    randomTransaction();
     updateReceiptData("raceName");
     updateReceiptData("raceTime");
     updateReceiptData("raceHorse");
-    updateReceiptData("raceOdds")
-    updateReceiptData("stake", format = "gbp")
+    updateReceiptData("raceOdds");
+    updateReceiptData("stake", format = "gbp");
+    updateBetType();
     $('#stake').keyup(function () {
         updateReturn()
     })
